c6e96b5e3e44bc798e45c839fcf6238c27e1ba1a
FabioCruz20 <fabiovict95@gmail.com>
2025-02-25 23:36:37 -0300

    feat: add user registration

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     # path('', include('django.contrib.auth.urls')),
     path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('register/', views.register_view, name='register'),
     path('profile/', views.profile_view, name='profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout
 
 # Create your views here.
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            login(request, form.save())
+            return redirect('profile')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'registration/register.html', { 'form': form })
+
 
 @login_required(login_url='accounts/login')
 def profile_view(request):
